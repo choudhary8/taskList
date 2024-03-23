55ed567e6d861acd020c33dee6eb87512bc3cd3e
Manoj <manojbhichchhar8@gmail.com>
2024-03-24 04:24:40 +0530

    backend frontend connection

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,14 @@
-let f=
+const tasksList=async ()=>{
+    let res=await axios.get("http://localhost:3000/");
+    let tasks=await res.data;
+        console.log(tasks);
+        for(let i=0;i<tasks.length;i++)
+        {
+            let ele=document.createElement("div");
+            ele.innerText=`${tasks[i]}`;
+            const n=document.getElementsByTagName("div");
+            n[0].appendChild(ele);
+        }
+}
+let getList=document.getElementsByTagName("button");
+getList[0].addEventListener("click",tasksList);
