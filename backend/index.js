07e0425dd0b07f68bbb88505589fa1ca51d478d1
Manoj <manojbhichchhar8@gmail.com>
2024-03-25 21:19:27 +0530

    sever connection error and utils making

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 const http=require('http');
 const app=require(`./src/app.js`);
-const connnectDB=require('./src/db/index.js')
+const connnectDB=require('./src/db/index.js');
+const { error } = require('console');
 require('dotenv').config()
 
 connnectDB();
 
-const startServer=async()=>{
-    const server=await http.createServer(app);
-    await server.listen(process.env.PORT,()=>console.log(`listning on ${process.env.PORT}`));
+try{
+    const server=http.createServer(app);
+    server.on("error",(error)=>{
+        throw error;
+    })
+    server.listen(process.env.PORT,()=>console.log(`listning on ${process.env.PORT}`));
+}catch(error)
+{
+    console.log("sever failed :", error);
 }
-startServer();
+
