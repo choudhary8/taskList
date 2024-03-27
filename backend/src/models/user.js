d1805267f4463196d89911ca58c16fe63a7883d1
Manoj <manojbhichchhar8@gmail.com>
2024-03-27 10:15:58 +0530

    accessToken& refreshToken and File upload section clodinary & multer

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,6 +1,10 @@
 const mongoose=require('mongoose');
+const bcrypt=require("bcrypt");
+const jwt = require('jsonwebtoken')
+const mongooseAggregatePaginate=require('mongoose-aggregate-paginate-v2')
 
-const userSchema=mongoose.Schema({
+const userSchema=mongoose.Schema(
+    {
     email:{
         type:String,
         require:true,
@@ -9,7 +13,54 @@ const userSchema=mongoose.Schema({
     password:{
         type:String,
         require:true
+    },
+    userPhoto:{
+        type:String
+    },
+    refreshToken:{
+        type:String
     }
+    },
+    {
+        timestamps:true
+    }
+
+)
+
+userSchema.pre("save",async function(next) {
+    if(!this.password.isModified) return next();
+    this.password=bcrypt.hash(this.password,10);
+    next();
 })
 
+userSchema.methods.isPasswordCorrect=async function(password){
+   return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXXPIRY
+        }
+    )
+}
+
+userSchema.plugin(mongooseAggregatePaginate)
 exports.module=mongoose.model("User",userSchema);
