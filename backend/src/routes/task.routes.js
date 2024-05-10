02cad10bcf32340ba0e3f7343cd34884eeb5fec8
Manoj <manojbhichchhar8@gmail.com>
2024-05-10 09:29:08 +0530

    router fot fetching all task for a user

--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -1,9 +1,10 @@
 const express=require('express');
-const { createTask } = require('../controllers/task.controller');
+const { createTask, getAllTask } = require('../controllers/task.controller');
 const verifyJWT = require('../middleware/auth.middleware');
 const router=express.Router();
 
 
 router.post('/create-task',createTask)
+router.get("/get-all-tasks",getAllTask)
 
 module.exports=router
